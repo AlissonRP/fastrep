f986a118d89f712109cbbbafb1ddc64696c87133
AlissonRP <alirpereira@hotmail.com>
2021-12-17 19:29:43 -0300

    change the marg argument

--- R/freq-table2.R
+++ R/freq-table2.R
@@ -8,19 +8,22 @@
 #' @param tit     Title for the table, write in string format
 #' @param v       Variable that you want the table (not written in string format)
 #' @param ...     Other arguments
+#' @param marg   Marginal row table
 #' @examples
 #'iris %>%
 #'mypdf1::pdf1_freq.tbl(Species,"title") %>%
 #' @export
-pdf1_freq.tbl2=function(obj,v1,v2,tit,marg){
-  if(missing(marg)){
-    title2=obj |>  dplyr::select({{v2}}) |> names()
-    catlev=nrow(unique(obj |>
-                         dplyr::select({{v2}})))+1
+pdf1_freq.tbl2=function(obj,v1,v2,tit,marg=F){
+  #if(missing(marg)){
     tab=obj %>%
       dplyr::group_by({{v1}},{{v2}}) %>%
       dplyr::summarise(n=dplyr::n())   %>%
-      tidyr::spread({{v2}}, n) %>%
+      tidyr::spread({{v2}}, n)
+      if(marg!=TRUE){
+        title2=obj |>  dplyr::select({{v2}}) |> names()
+        catlev=nrow(unique(obj |>
+                             dplyr::select({{v2}})))+1
+      tab=tab |>
       adorn_totals("row") %>%
       adorn_totals("col") %>%
       dplyr::ungroup() |>
@@ -32,10 +35,7 @@ pdf1_freq.tbl2=function(obj,v1,v2,tit,marg){
     title2=obj |>  dplyr::select({{v2}}) |> names()
     catlev=nrow(unique(obj |>
                          dplyr::select({{v2}})))
-    tab=obj %>%
-      dplyr::group_by({{v1}},{{v2}}) %>%
-      dplyr::summarise(n=dplyr::n())   %>%
-      tidyr::spread({{v2}}, n) %>%
+    tab=tab |>
       adorn_percentages() %>%
       dplyr::ungroup() |>
       mypdf1::pdf1_tbl(tit) |>

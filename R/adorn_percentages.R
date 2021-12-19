627106bb912d1d6c3fbc6b69f6335c4729a6cf17
AlissonRP <alirpereira@hotmail.com>
2021-12-18 23:08:05 -0300

    table fixed position

---
+++ R/adorn_percentages.R
@@ -0,0 +1,77 @@
+adorn_percentages <- function(dat, denominator = "row", na.rm = TRUE, ...) {
+  # if input is a list, call purrr::map to recursively apply this function to each data.frame
+  if (is.list(dat) && !is.data.frame(dat)) {
+    purrr::map(dat, adorn_percentages, denominator, na.rm, ...)
+  } else {
+    # catch bad inputs
+    if (!is.data.frame(dat)) {
+      stop("adorn_percentages() must be called on a data.frame or list of data.frames")
+    }
+    if (!denominator %in% c("row", "col", "all")) {
+      stop("'denominator' must be one of 'row', 'col', or 'all'")
+    }
+
+    dat <- as_tabyl(dat)
+
+    numeric_cols <- which(vapply(dat, is.numeric, logical(1)))
+    non_numeric_cols <- setdiff(1:ncol(dat), numeric_cols)
+    numeric_cols <- setdiff(numeric_cols, 1) # assume 1st column should not be included so remove it from numeric_cols. Moved up to this line so that if only 1st col is numeric, the function errors
+    explicitly_exempt_totals <- FALSE
+
+    if(rlang::dots_n(...) == 0){
+      cols_to_tally <- numeric_cols
+    } else {
+      expr <- rlang::expr(c(...))
+      cols_to_tally <- tidyselect::eval_select(expr, data = dat)
+      explicitly_exempt_totals <- !(ncol(dat) %in% cols_to_tally) # if not present, it's b/c user explicitly exempted it
+      if(any(cols_to_tally %in% non_numeric_cols)){
+        message("At least one non-numeric column was specified.  All non-numeric columns will be removed from percentage calculations.")
+        cols_to_tally <- setdiff(cols_to_tally, non_numeric_cols)
+      }
+    }
+
+    if ("col" %in% attr(dat, "totals")) {
+      # if there's a totals col, don't use it to calculate the %s
+      cols_to_tally <- setdiff(cols_to_tally, ncol(dat))
+    }
+
+    if (denominator == "row") {
+      # if row-wise percentages and a totals column, need to exempt totals col and make it all 1s
+      if ("col" %in% attr(dat, "totals") & !explicitly_exempt_totals) {
+        dat[[ncol(dat)]] <- rep(1, nrow(dat))
+      }
+      row_sum <- rowSums(dat[cols_to_tally], na.rm = na.rm)
+      dat[, cols_to_tally] <- dat[cols_to_tally] / row_sum
+    } else if (denominator == "col") {
+      # if col-wise percentages and a row column, need to exempt totals row and make it all 1s
+      if ("row" %in% attr(dat, "totals")) {
+        col_sum <- colSums(dat[-nrow(dat), ][cols_to_tally], na.rm = na.rm)
+      } else {
+        col_sum <- colSums(dat[cols_to_tally], na.rm = na.rm)
+      }
+      # add totals col back to be tallied, #357
+      if ("col" %in% attr(dat, "totals") & !explicitly_exempt_totals) {
+        cols_to_tally <- c(cols_to_tally, ncol(dat))
+        if ("row" %in% attr(dat, "totals")) {
+          col_sum <- c(col_sum, sum(dplyr::last(dat)[-nrow(dat)]))
+        } else {
+          col_sum <- c(col_sum, sum(dplyr::last(dat)))
+        }
+      }
+      dat[cols_to_tally] <- sweep(dat[cols_to_tally], 2, col_sum, `/`) # from http://stackoverflow.com/questions/9447801/dividing-columns-by-colsums-in-r
+    } else if (denominator == "all") {
+      # if all-wise percentages, need to exempt any totals col or row
+      if ("row" %in% attr(dat, "totals")) {
+        complete_n <- sum(dat[-nrow(dat), cols_to_tally], na.rm = TRUE)
+      } else {
+        complete_n <- sum(dat[, cols_to_tally], na.rm = TRUE)
+      }
+      # add totals col back to be tallied, #357
+      if ("col" %in% attr(dat, "totals") & !explicitly_exempt_totals) {
+        cols_to_tally <- c(cols_to_tally, ncol(dat))
+      }
+      dat[cols_to_tally] <- dat[cols_to_tally] / complete_n
+    }
+    dat
+  }
+}

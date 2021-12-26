73432930b423a9f33a8ddc4353b7012b016c4933
AlissonRP <alirpereira@hotmail.com>
2021-12-26 14:38:00 -0300

    tibble in pdf1-na

--- R/pdf1_na.R
+++ R/pdf1_na.R
@@ -1,8 +1,18 @@
-
-
-
-pdf1_na=function(df){
-  is.na(df) |>
+#' pdf1_na
+#'
+#'this is a very simple frequency table generator
+#'
+#'
+#' @param obj     Object used to create the table. Data frame, list or environment
+#' (or object coercible by as.data.frame to a data frame)
+#' @param ...     Other arguments
+#' @examples
+#'airquality %>%
+#'mypdf1::pdf1_na() %>%
+#' @export
+pdf1_na=function(obj,...){
+  vec=is.na(obj) |>
     as.data.frame() |>
     purrr::map_dbl(sum)
+dplyr::tibble(`Variável`=names(vec),`Total_Na`=vec %>%  as.vector())
 }

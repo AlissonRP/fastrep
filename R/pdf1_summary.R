42e952236ed682d601d76b17236ad357f7a479c2
AlissonRP <alirpereira@hotmail.com>
2022-01-20 02:19:21 -0300

    more test in `pdf1_summary`

--- R/pdf1_summary.R
+++ R/pdf1_summary.R
@@ -5,17 +5,38 @@
 #'
 #' @param obj     Object used to create the table. Data frame, list or environment
 #' (or object coercible by as.data.frame to a data frame)
+#' @param na_rm option to remove NA from variables
+#' @param use_num option to use only numeric variables
 
 #' @examples
 #' mtcars |>
 #'   mypdf1::pdf1_summary()
+#'
+#' airquality |> pdf1_summary(na_rm = F)
+#'
+#' iris |> mypdf1::pdf1_summary(use_num = T)
 #' @export
-pdf1_summary <- function(obj, na = TRUE) {
-  obj <- obj |>
-    dplyr::select(where(is.numeric))
+pdf1_summary <- function(obj, na_rm = TRUE, use_num = FALSE) {
+  if (use_num == TRUE) {
+    obj <- obj |>
+      dplyr::select(where(is.numeric))
+  } else if (3 > 2) {
+    char <- obj |>
+      dplyr::select(where((is.character))) |> # dumb i know
+      ncol()
+    fac <- obj |>
+      dplyr::select(where((is.factor))) |> # dumb i know
+      ncol()
+    if ((char + fac) != 0) {
+      stop("Summary only for numeric variables \n use use_num=TRUE")
+    }
+  }
+
+
+
 
   funs <- c(mean = mean, median = median, sd = sd, min = min, max = max)
-  args <- list(na.rm = na)
+  args <- list(na.rm = na_rm)
   obj |>
     purrr::map_df(~ funs %>%
       purrr::map(purrr::exec, .x, !!!args), .id = "var")
--- tests/testthat/test_summary.R
+++ tests/testthat/test_summary.R
@@ -9,4 +9,11 @@ test_that("sum mean", {
     mtcars |> mypdf1::pdf1_summary() |> (\(x) x[5, 4])() |> as.numeric() |> round(1),
     round(sd(mtcars$drat), 1)
   )
+  expect_error(
+    iris |> mypdf1::pdf1_summary(), "Summary only for numeric variables \n use use_num=TRUE"
+  )
+  expect_equal(
+    iris |> mypdf1::pdf1_summary(use_num = T) |> (\(x) x[1, 2])() |> as.numeric() |> round(1),
+    round(mean(iris$Sepal.Length), 1)
+  )
 })

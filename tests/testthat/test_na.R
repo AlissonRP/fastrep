83e4c9f94e6591ff53c2f7701ef5c837be7f6ac8
AlissonRP <alirpereira887@gmail.com>
2022-05-27 01:27:12 -0300

    vignettes and change in pdf1_na

--- tests/testthat/test_na.R
+++ tests/testthat/test_na.R
@@ -2,6 +2,6 @@
 library(mypdf1)
 
 test_that("sum na's", {
-  expect_equal(airquality %>% pdf1_na() %>% dplyr::select("total_Na") %>% sum(), 44)
-  expect_equal(iris %>% pdf1_na() %>% dplyr::select("total_Na") %>% sum(), 0)
+  expect_equal(airquality %>% pdf1_na() %>% dplyr::select("total_NA") %>% sum(), 44)
+  expect_equal(iris %>% pdf1_na() %>% dplyr::select("total_NA") %>% sum(), 0)
 })
